205b6b0f7fff0c2c49374ad0a91c5105f348a724
Kubanych22 <z200707@mail.ru>
2024-05-30 15:09:00 +0600

    fixed API.js

--- src/scripts/API.js
+++ src/scripts/API.js
@@ -2,8 +2,6 @@ import { productStore } from "./Store";
 
 export const API_URL = 'https://outgoing-strong-nail.glitch.me';
 // export const API_URL = "https://mirano-api-9k3t.onrender.com";
-// export const API_URL = 'https://outgoing-strong-nail.glitch.me';
-
 // export const API_URL = 'http://localhost:3000';
 
 const formatQueryString = (params) => {
